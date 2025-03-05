83dfe655cf36f693360e0aaa9c7142606dbfcba6
mozzamaru <kanghyun486111@gmail.com>
2025-03-05 20:56:56 +0900

    ✨HyperZombie플러그인 패치!

--- HyperZombie/src/main/kotlin/org/kotlin/hyperZombie/HyperZombie.kt
+++ HyperZombie/src/main/kotlin/org/kotlin/hyperZombie/HyperZombie.kt
@@ -1,68 +1,45 @@
-package io.github.mozza.org.kotlin.hyperzobie
+package org.kotlin.hyperZombie
+    import org.bukkit.Bukkit
+    import org.bukkit.attribute.Attribute
+    import org.bukkit.entity.EntityType
+    import org.bukkit.entity.LivingEntity
+    import org.bukkit.entity.Zombie
+    import org.bukkit.event.EventHandler
+    import org.bukkit.event.Listener
+    import org.bukkit.event.entity.EntityDamageByEntityEvent
+    import org.bukkit.plugin.java.JavaPlugin
+    import org.bukkit.scheduler.BukkitRunnable
 
-import org.bukkit.Material
-import org.bukkit.entity.Creeper
-import org.bukkit.entity.EntityType
-import org.bukkit.entity.LivingEntity
-import org.bukkit.entity.Player
-import org.bukkit.entity.Projectile
-import org.bukkit.entity.Zombie
-import org.bukkit.event.EventHandler
-import org.bukkit.event.Listener
-import org.bukkit.event.entity.CreatureSpawnEvent
-import org.bukkit.event.entity.EntityDamageByEntityEvent
-import org.bukkit.event.entity.EntityDeathEvent
-import org.bukkit.event.entity.ProjectileHitEvent
-import org.bukkit.potion.PotionEffect
-import org.bukkit.potion.PotionEffectType
-import org.bukkit.plugin.java.JavaPlugin
-
-class HyperZombie : JavaPlugin(), Listener {
-
-    override fun onEnable() {
-        // Listener 등록
-        server.pluginManager.registerEvents(this, this)
-    }
-
-    @EventHandler
-    fun onZombieSpawn(event: CreatureSpawnEvent) {
-        if (event.entity.type == EntityType.ZOMBIE) {
-            val zombie = event.entity as Zombie
-            zombie.setBaby() // 작은 크기의 아기 좀비로 설정
-
-            // 좀비에게 투명 효과 부여 (지속 시간 무제한 대신 긴 시간 설정)
-            zombie.addPotionEffect(PotionEffect(PotionEffectType.INVISIBILITY, 1000000, 1)) // 예시로 긴 지속 시간 설정
-            zombie.addPotionEffect(PotionEffect(PotionEffectType.FIRE_RESISTANCE, 1000000, 1))
-            zombie.addPotionEffect(PotionEffect(PotionEffectType.REGENERATION, 1000000, 1))
-            zombie.addPotionEffect(PotionEffect(PotionEffectType.STRENGTH, 1000000, 1))
-            zombie.addPotionEffect(PotionEffect(PotionEffectType.HEALTH_BOOST, 1000000, 1))
+    class HyperZombie : JavaPlugin(), Listener {
+        override fun onEnable() {
+            logger.info("FastZombiePlugin has been enabled!")
+            Bukkit.getPluginManager().registerEvents(this, this)
         }
-    }
 
-    @EventHandler
-    fun onZombieAttack(event: EntityDamageByEntityEvent) {
-        val damager = event.damager // 공격자
-        if (damager is Zombie) {
-            // 좀비가 플레이어나 다른 엔티티를 공격하면 Glow 효과 적용
-            damager.addPotionEffect(PotionEffect(PotionEffectType.GLOWING, 20, 1)) // 40틱 = 2초
-            damager.addPotionEffect(PotionEffect(PotionEffectType.SLOWNESS, 40, 1))
+        override fun onDisable() {
+            logger.info("FastZombiePlugin has been disabled!")
         }
-    }
-}
-@EventHandler
-fun onProjectileHit(event: ProjectileHitEvent) {
-    val projectile = event.entity
 
-    if (projectile is Projectile && event.hitEntity is LivingEntity) {
-        val hitEntity = event.hitEntity as LivingEntity
-        // 발사체의 발사자가 플레이어인지 확인
-        if (projectile.shooter is Player) {
-            val player = projectile.shooter as Player
-            // 타격당한 크리퍼가 맞았을 경우
-            if (hitEntity is Zombie) {
-                // 크리퍼를 발사자 위치로 텔레포트
-                hitEntity.teleport(player.location)
+        @EventHandler
+        fun onZombieAttack(event: EntityDamageByEntityEvent) {
+            val damager = event.damager
+            val target = event.entity
+
+            if (damager is Zombie && target is LivingEntity) {
+                event.isCancelled = true // 기본 공격 무효화
+                val damage = damager.getAttribute(Attribute.ATTACK_DAMAGE)?.value ?: 4.0
+                target.noDamageTicks = 0 // 무적 시간 제거
+
+                object : BukkitRunnable() {
+                    override fun run() {
+                        if (!damager.isDead && !target.isDead) {
+                            target.damage(damage, damager)
+                            target.noDamageTicks = 0 // 지속적으로 무적 시간 제거
+                        } else {
+                            cancel()
+                        }
+                    }
+                }.runTaskTimer(this, 0L, 1L) // 1틱마다 피해 적용
             }
         }
     }
-}
